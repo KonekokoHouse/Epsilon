8daf11592bedbec359242c5a068df021a6241c12
L3MonKe <hhcrystal2008@gmail.com>
2026-05-23 19:42:31 +0800

    yes

--- common/src/main/java/com/github/epsilon/modules/impl/combat/SilentAim.java
+++ common/src/main/java/com/github/epsilon/modules/impl/combat/SilentAim.java
@@ -13,11 +13,11 @@ import com.github.epsilon.settings.impl.DoubleSetting;
 import com.github.epsilon.settings.impl.IntSetting;
 import com.github.epsilon.utils.rotation.Priority;
 import com.github.epsilon.utils.rotation.RotationUtils;
+import com.github.epsilon.utils.rotation.Rot2f;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.HitResult;
-import org.joml.Vector2f;
 
 public class SilentAim extends Module {
 
@@ -53,7 +53,7 @@ public class SilentAim extends Module {
             return;
         }
 
-        Vector2f rotations = RotationUtils.calculate(target.getEyePosition());
+        Rot2f rotations = RotationUtils.calculate(target.getEyePosition());
         RotationManager.INSTANCE.setRotations(rotations, 10, Priority.High);
 
         if (mc.hitResult != null && mc.hitResult.getType() == HitResult.Type.ENTITY) {
--- common/src/main/java/com/github/epsilon/modules/impl/movement/Phase.java
+++ common/src/main/java/com/github/epsilon/modules/impl/movement/Phase.java
@@ -15,6 +15,7 @@ import com.github.epsilon.utils.player.FindItemResult;
 import com.github.epsilon.utils.player.InvUtils;
 import com.github.epsilon.utils.player.MoveUtils;
 import com.github.epsilon.utils.rotation.RotationUtils;
+import com.github.epsilon.utils.rotation.Rot2f;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
@@ -27,7 +28,6 @@ import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.HitResult;
-import org.joml.Vector2f;
 
 public class Phase extends Module {
 
@@ -202,7 +202,7 @@ public class Phase extends Module {
                     return;
                 }
 
-                Vector2f angle = RotationUtils.calculate(block.getCenter());
+                Rot2f angle = RotationUtils.calculate(block.getCenter());
                 FindItemResult result = swapMode.is(SwapMode.Silent) ? InvUtils.findInHotbar(Items.ENDER_PEARL) : InvUtils.find(Items.ENDER_PEARL);
                 if (result.found()) {
                     float prevYaw = mc.player.getYRot();
