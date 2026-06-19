a978e305c8c87a6e16625358ef7eaddfea915f3a
L3MonKe178 <hhcrystal2008@gmail.com>
2026-06-19 12:46:04 +0800

    已严肃修复 ChestESP

--- common/src/main/java/com/github/epsilon/mixins/MixinFeatureRenderDispatcherPhaseSubmitGrouper.java
+++ common/src/main/java/com/github/epsilon/mixins/MixinFeatureRenderDispatcherPhaseSubmitGrouper.java
@@ -6,10 +6,10 @@ import net.minecraft.client.renderer.feature.submit.SubmitNode;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 import java.util.Collection;
 import java.util.List;
