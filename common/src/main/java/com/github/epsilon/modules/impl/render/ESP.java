e85872924772a20a11159a20fc3e7ac51aeadabd
L3MonKe178 <hhcrystal2008@gmail.com>
2026-06-20 22:41:19 +0800

    b

--- common/src/main/java/com/github/epsilon/modules/impl/render/ESP.java
+++ common/src/main/java/com/github/epsilon/modules/impl/render/ESP.java
@@ -1,6 +1,7 @@
 package com.github.epsilon.modules.impl.render;
 
 import com.github.epsilon.events.bus.EventHandler;
+import com.github.epsilon.events.impl.PlayerTickEvent;
 import com.github.epsilon.events.impl.Render3DEvent;
 import com.github.epsilon.managers.Managers;
 import com.github.epsilon.modules.Category;
@@ -9,21 +10,22 @@ import com.github.epsilon.settings.impl.BlockListSetting;
 import com.github.epsilon.settings.impl.BoolSetting;
 import com.github.epsilon.settings.impl.ColorSetting;
 import com.github.epsilon.settings.impl.DoubleSetting;
+import com.github.epsilon.utils.timer.TimerUtils;
+import com.google.common.collect.Lists;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.ChestBlock;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.util.Mth;
+import net.minecraft.util.Util;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.ChestType;
-import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
 import java.awt.*;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ESP extends Module {
 
@@ -33,8 +35,8 @@ public class ESP extends Module {
         super("ESP", Category.RENDER);
     }
 
-    private final BoolSetting blocks = boolSetting("Blocks", true);
-    private final BlockListSetting blockList = blockListSetting("Block List",
+    private final BoolSetting blocksValue = boolSetting("Blocks", true);
+    private final BlockListSetting blockListValue = blockListSetting("Block List",
             List.of(
                     Blocks.CHEST,
                     Blocks.TRAPPED_CHEST,
@@ -57,94 +59,113 @@ public class ESP extends Module {
                     Blocks.GREEN_SHULKER_BOX,
                     Blocks.RED_SHULKER_BOX,
                     Blocks.BLACK_SHULKER_BOX
-            ), blocks::getValue);
+            ), blocksValue::getValue);
+    private final BoolSetting illegals = boolSetting("Illegals", true);
     private final DoubleSetting range = doubleSetting("Range", 64.0, 1.0, 128.0, 1.0);
-    private final ColorSetting color = colorSetting("Color", new Color(160, 210, 255, 30));
+    private final ColorSetting sideColor = colorSetting("Side Color", new Color(160, 210, 255, 30));
+    private final ColorSetting lineColor = colorSetting("Line Color", new Color(160, 210, 255, 180));
     private final BoolSetting blur = boolSetting("Blur", true);
     private final DoubleSetting blurStrength = doubleSetting("Blur Strength", 5.0, 0.0, 16.0, 0.5, blur::getValue);
 
+    private final ExecutorService searchThread = Executors.newSingleThreadExecutor();
+    private final TimerUtils searchTimer = new TimerUtils();
+    private boolean canContinue;
+
+    public static List<BlockVec> blocks = new ArrayList<>();
+
+    @Override
+    protected void onEnable() {
+        blocks.clear();
+        canContinue = true;
+    }
+
+    @EventHandler
+    private void onPlayerTick(PlayerTickEvent event) {
+        if (searchTimer.every(1000) && canContinue) {
+            CompletableFuture.supplyAsync(this::scan, searchThread).thenAcceptAsync(this::sync, Util.backgroundExecutor());
+            canContinue = false;
+        }
+    }
+
     @EventHandler
     private void onRender3D(Render3DEvent event) {
-        if (blocks.getValue()) {
-            double maxRange = range.getValue();
-            int renderDistance = mc.options.renderDistance().get();
+        if (blocks.isEmpty()) return;
 
-            BlockPos playerPos = mc.player.blockPosition();
-            ChunkPos playerChunk = mc.player.chunkPosition();
-            Set<Block> selectedBlocks = blockList.asSet();
-            Set<BlockPos> renderedBlocks = new HashSet<>();
-
-            if (selectedBlocks.isEmpty()) {
-                return;
+        for (BlockVec vec : Lists.newArrayList(blocks)) {
+            if (vec.getDistance(mc.player.position()) > range.getValue()) {
+                blocks.remove(vec);
+                continue;
             }
 
-            for (int x = -renderDistance; x <= renderDistance; x++) {
-                for (int z = -renderDistance; z <= renderDistance; z++) {
-                    int chunkX = playerChunk.x() + x;
-                    int chunkZ = playerChunk.z() + z;
-                    if (!mc.level.hasChunk(chunkX, chunkZ)) {
-                        continue;
-                    }
+            AABB b = new AABB(vec.x, vec.y, vec.z, vec.x + 1, vec.y + 1, vec.z + 1);
 
-                    LevelChunk chunk = mc.level.getChunkSource().getChunkNow(chunkX, chunkZ);
-                    if (chunk == null) {
-                        continue;
-                    }
-
-                    for (BlockEntity entity : chunk.getBlockEntities().values()) {
-                        BlockPos blockPos = entity.getBlockPos();
-                        BlockState state = mc.level.getBlockState(blockPos);
-                        if (!selectedBlocks.contains(state.getBlock())) {
-                            continue;
-                        }
-                        renderBlock(blockPos, state, selectedBlocks, renderedBlocks, playerPos, maxRange);
-                    }
-                }
-            }
-
-            int blockRange = (int) Math.ceil(maxRange);
-            int minY = mc.level.getMinY();
-            int maxY = mc.level.getMaxY();
-            BlockPos min = new BlockPos(playerPos.getX() - blockRange, Math.max(minY, playerPos.getY() - blockRange), playerPos.getZ() - blockRange);
-            BlockPos max = new BlockPos(playerPos.getX() + blockRange, Math.min(maxY, playerPos.getY() + blockRange), playerPos.getZ() + blockRange);
-            for (BlockPos pos : BlockPos.betweenClosed(min, max)) {
-                if (!mc.level.isLoaded(pos) || renderedBlocks.contains(pos)) {
-                    continue;
-                }
-                BlockState state = mc.level.getBlockState(pos);
-                if (selectedBlocks.contains(state.getBlock())) {
-                    renderBlock(pos.immutable(), state, selectedBlocks, renderedBlocks, playerPos, maxRange);
-                }
-            }
+            if (blur.getValue()) Managers.RENDER.addBlurredBox(b, blurStrength.getValue());
+            Managers.RENDER.addFilledBox(b, sideColor.getValue());
+            Managers.RENDER.addOutlineBox(b, lineColor.getValue());
         }
     }
 
-    private void renderBlock(BlockPos blockPos, BlockState state, Set<Block> selectedBlocks, Set<BlockPos> renderedBlocks, BlockPos playerPos, double maxRange) {
-        if (blockPos.distSqr(playerPos) > maxRange * maxRange || !renderedBlocks.add(blockPos)) {
-            return;
-        }
+    private List<BlockVec> scan() {
+        List<BlockVec> blocks = new ArrayList<>();
+        int startX = Mth.floor(mc.player.getX() - range.getValue());
+        int endX = Mth.ceil(mc.player.getX() + range.getValue());
+        int startY = mc.level.getMinY() + 1;
+        int endY = mc.level.getMaxY();
+        int startZ = Mth.floor(mc.player.getZ() - range.getValue());
+        int endZ = Mth.ceil(mc.player.getZ() + range.getValue());
 
-        AABB box = getAABB(blockPos);
-        if (state.getBlock() instanceof ChestBlock && state.getValue(ChestBlock.TYPE) != ChestType.SINGLE) {
-            BlockPos connectedPos = ChestBlock.getConnectedBlockPos(blockPos, state);
-            if (mc.level.isLoaded(connectedPos)) {
-                BlockState connectedState = mc.level.getBlockState(connectedPos);
-                if (selectedBlocks.contains(connectedState.getBlock())
-                        && connectedState.getBlock() == state.getBlock()
-                        && connectedState.getValue(ChestBlock.TYPE) == state.getValue(ChestBlock.TYPE).getOpposite()
-                        && connectedState.getValue(ChestBlock.FACING) == state.getValue(ChestBlock.FACING)) {
-                    box = box.minmax(getAABB(connectedPos));
-                    renderedBlocks.add(connectedPos);
+        for (int x = startX; x <= endX; x++) {
+            for (int y = startY; y <= endY; y++) {
+                for (int z = startZ; z <= endZ; z++) {
+                    BlockPos pos = new BlockPos(x, y, z);
+                    BlockState bs = mc.level.getBlockState(pos);
+                    if (shouldAdd(bs.getBlock(), pos)) {
+                        blocks.add(new BlockVec(pos.getX(), pos.getY(), pos.getZ()));
+                    }
                 }
             }
         }
-
-        if (blur.getValue()) Managers.RENDER.addBlurredBox(box, blurStrength.getValue());
-        Managers.RENDER.addFilledBox(box, color.getValue());
+        return blocks;
     }
 
-    private AABB getAABB(BlockPos blockPos) {
-        return mc.level.getBlockState(blockPos).getShape(mc.level, blockPos).bounds().move(blockPos);
+    private void sync(List<BlockVec> b) {
+        blocks = b;
+        canContinue = true;
+    }
+
+    private boolean shouldAdd(Block block, BlockPos pos) {
+        if (block instanceof AirBlock) return false;
+        if (blockListValue.getValue().contains(block)) return true;
+        if (illegals.getValue()) return isIllegal(block, pos);
+        return false;
+    }
+
+    private boolean isIllegal(Block block, BlockPos pos) {
+        if (block instanceof CommandBlock || block instanceof BarrierBlock) return true;
+
+        if (block == Blocks.BEDROCK) {
+            if (false/*!mc.level.getEntityInAnyDimension()*/) {
+                return pos.getY() > 4;
+            } else {
+                return pos.getY() > 127 || (pos.getY() < 123 && pos.getY() > 4);
+            }
+        }
+        return false;
+    }
+
+    public record BlockVec(double x, double y, double z) {
+
+        public double getDistance(Vec3 v) {
+            double dx = x - v.x;
+            double dy = y - v.y;
+            double dz = z - v.z;
+            return dx * dx + dy * dy + dz * dz;
+        }
+
+        public Vec3 getVector() {
+            return new Vec3(x + 0.5f, y + 0.5f, z + 0.5f);
+        }
+
     }
 
 }
