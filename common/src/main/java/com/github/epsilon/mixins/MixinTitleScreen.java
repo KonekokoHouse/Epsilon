a81b0c7f710c789dd868bbdb5d36918120df5eab
Chen Meng <chenmeng466@outlook.com>
2026-06-20 13:35:26 +0800

    [26.2.x] Backport #282: 添加 Welcome Screen
    Cherry-pick of 2532907bc932098d72964bd09fa9e74752a39965 produced conflicts. Resolve the conflict markers manually before merging this backport PR.

--- common/src/main/java/com/github/epsilon/mixins/MixinTitleScreen.java
+++ common/src/main/java/com/github/epsilon/mixins/MixinTitleScreen.java
@@ -2,6 +2,7 @@ package com.github.epsilon.mixins;
 
 import com.github.epsilon.Constants;
 import com.github.epsilon.gui.screen.MainMenuScreen;
+import com.github.epsilon.gui.screen.WelcomeScreen;
 import com.github.epsilon.modules.impl.ClientSetting;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,6 +17,7 @@ import static com.github.epsilon.Constants.mc;
 public class MixinTitleScreen {
 
     @Unique
+<<<<<<< HEAD
     private static boolean epsilon$freeNoticeShown;
 
     @Inject(method = "init", at = @At("HEAD"), cancellable = true)
@@ -23,6 +25,19 @@ public class MixinTitleScreen {
         if (!epsilon$freeNoticeShown) {
             epsilon$freeNoticeShown = true;
             Constants.LOGGER.warn("Epsilon 客户端完全免费，请勿向任何人付费购买。官方群：3787275604");
+=======
+    private static boolean epsilon$welcomeHandled;
+
+    @Inject(method = "init", at = @At("HEAD"), cancellable = true)
+    private void onInit(CallbackInfo ci) {
+        if (!epsilon$welcomeHandled) {
+            epsilon$welcomeHandled = true;
+            if (ClientSetting.INSTANCE.showWelcomeScreen.getValue()) {
+                ci.cancel();
+                mc.setScreen(WelcomeScreen.INSTANCE);
+                return;
+            }
+>>>>>>> 2532907 (添加 Welcome Screen (#282))
         }
 
         if (ClientSetting.INSTANCE.useMainMenu.getValue()) {
