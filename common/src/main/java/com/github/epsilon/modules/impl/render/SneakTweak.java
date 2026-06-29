416165a8bb71d675293045b3119b778e7ea35ffc
L3MonKe178 <hhcrystal2008@gmail.com>
2026-06-29 15:44:40 +0800

    Update SneakTweak.java

--- common/src/main/java/com/github/epsilon/modules/impl/render/SneakTweak.java
+++ common/src/main/java/com/github/epsilon/modules/impl/render/SneakTweak.java
@@ -1,21 +1,18 @@
 package com.github.epsilon.modules.impl.render;
 
-import com.github.epsilon.events.bus.EventHandler;
-import com.github.epsilon.events.impl.PlayerTickEvent;
 import com.github.epsilon.modules.Category;
 import com.github.epsilon.modules.Module;
 import com.github.epsilon.settings.impl.BoolSetting;
 import com.github.epsilon.settings.impl.DoubleSetting;
 import com.github.epsilon.settings.impl.EnumSetting;
-import com.github.epsilon.settings.impl.IntSetting;
-import net.minecraft.client.CameraType;
-import net.minecraft.world.entity.EntityDimensions;
-import net.minecraft.world.entity.Pose;
-import net.minecraft.world.entity.player.Player;
 
 public class SneakTweak extends Module {
 
     public static final SneakTweak INSTANCE = new SneakTweak();
+
+    private SneakTweak() {
+        super("Sneak Tweak", Category.RENDER);
+    }
 
     private enum SneakingEyeHeight {
         Default,
@@ -25,11 +22,7 @@ public class SneakTweak extends Module {
     }
 
     private final EnumSetting<SneakingEyeHeight> sneakingEyeHeight = enumSetting("Sneaking Eye Height", SneakingEyeHeight.Default);
-    private final DoubleSetting customSneakingEyeHeight = doubleSetting("Custom Sneaking Eye Height", 1.27, 0.0, 1.8, 0.01, () -> sneakingEyeHeight.is(SneakingEyeHeight.Custom), _ -> refreshPlayerDimensions());
-    private final BoolSetting thirdPersonEyeHeight = boolSetting("Third Person Eye Height", true, _ -> refreshPlayerDimensions());
-
-    private SneakTweak() {
-        super("Sneak Tweak", Category.RENDER);
-    }
+    private final DoubleSetting customSneakingEyeHeight = doubleSetting("Custom Sneaking Eye Height", 1.27, 0.0, 1.8, 0.01, () -> sneakingEyeHeight.is(SneakingEyeHeight.Custom));
+    private final BoolSetting thirdPersonEyeHeight = boolSetting("Third Person Eye Height", true);
 
 }
