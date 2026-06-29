6ecdb63a3749acc105fd9b0cfc0e95a3e42fef23
Chen Meng <chenmeng466@outlook.com>
2026-06-29 14:18:55 +0800

    [26.2.x] Backport #333: 优化 GUI 打开动画和字体渲染器性能
    Cherry-pick of 39383183a9b7a637031c6536036d926a281dbfeb produced conflicts. Resolve the conflict markers manually before merging this backport PR.

--- common/src/main/java/com/github/epsilon/mixins/MixinRenderSystem.java
+++ common/src/main/java/com/github/epsilon/mixins/MixinRenderSystem.java
@@ -1,7 +1,14 @@
 package com.github.epsilon.mixins;
 
 import com.github.epsilon.graphics.LuminRenderSystem;
+<<<<<<< HEAD
 import net.minecraft.client.renderer.DynamicUniforms;
+=======
+import com.github.epsilon.graphics.text.ttf.TtfFontLoader;
+import com.mojang.blaze3d.TracyFrameCapture;
+import com.mojang.blaze3d.systems.RenderSystem;
+import org.jspecify.annotations.Nullable;
+>>>>>>> 39383183 (优化 GUI 打开动画和字体渲染器性能 (#333))
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,6 +20,7 @@ public class MixinRenderSystem {
     @Inject(method = "reset", at = @At("RETURN"))
     private void onReset(CallbackInfo ci) {
         LuminRenderSystem.endDynamicUniformFrame();
+        TtfFontLoader.beginRenderFrame();
     }
 
 }
