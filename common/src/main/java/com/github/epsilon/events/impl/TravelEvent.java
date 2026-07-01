e379da8d8e87feac21844ba7f83cfca0f3645de7
liuliuliu0127 <xyyysonic@163.com>
2026-07-02 02:10:03 +0800

    https://github.com/NekoyaHouse/Epsilon/pull/354参考该建议

--- common/src/main/java/com/github/epsilon/events/impl/TravelEvent.java
+++ common/src/main/java/com/github/epsilon/events/impl/TravelEvent.java
@@ -7,11 +7,9 @@ public class TravelEvent extends Cancellable {
     private static final TravelEvent INSTANCE = new TravelEvent();
 
     public Vec3 move;
-    public boolean isCancel;
 
     public static TravelEvent get(Vec3 move) {
         INSTANCE.move = move;
-        INSTANCE.isCancel = false;
         return INSTANCE;
     }
 }
