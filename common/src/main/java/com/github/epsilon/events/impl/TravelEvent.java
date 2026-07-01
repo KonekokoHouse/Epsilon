6bc94e36b359f62f268a25c74059594904672044
liuliuliu0127 <xyyysonic@163.com>
2026-07-01 20:17:51 +0800

    添加travelevent具体内容以及相应的mixin注入

--- common/src/main/java/com/github/epsilon/events/impl/TravelEvent.java
+++ common/src/main/java/com/github/epsilon/events/impl/TravelEvent.java
@@ -1,6 +1,17 @@
 package com.github.epsilon.events.impl;
 
 import com.github.epsilon.events.bus.Cancellable;
+import net.minecraft.world.phys.Vec3;
 
 public class TravelEvent extends Cancellable {
+    private static final TravelEvent INSTANCE = new TravelEvent();
+
+    public Vec3 move;
+    public boolean isCancel;
+
+    public static TravelEvent get(Vec3 move) {
+        INSTANCE.move = move;
+        INSTANCE.isCancel = false;
+        return INSTANCE;
+    }
 }
