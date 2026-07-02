2362cf8040669cb442d55e3eeb30a60dbe4eed3e
liuliuliu0127 <xyyysonic@163.com>
2026-07-02 13:06:36 +0800

    删除错误加入的settingtest

--- common/src/main/java/com/github/epsilon/holders/ModuleHolder.java
+++ common/src/main/java/com/github/epsilon/holders/ModuleHolder.java
@@ -83,7 +83,6 @@ public class ModuleHolder {
         addModule(Stealer.INSTANCE);
         addModule(UseCooldown.INSTANCE);
         addModule(AutoQueue.INSTANCE);
-        addModule(SettingsTest.INSTANCE);
 
         // Movement
         addModule(ElytraFly.INSTANCE);
