71b811770e72b9434e6207f53e8c4e05ebb5b006
liuliuliu0127 <xyyysonic@163.com>
2026-07-02 19:51:07 +0800

    修复waterfall gui设置选项渲染问题

--- common/src/main/java/com/github/epsilon/gui/dropdown/widget/ScreenHandlerListSettingWidget.java
+++ common/src/main/java/com/github/epsilon/gui/dropdown/widget/ScreenHandlerListSettingWidget.java
@@ -32,10 +32,10 @@ public class ScreenHandlerListSettingWidget extends SettingWidget<ScreenHandlerL
         boolean hovered = isFieldHovered(mouseX, mouseY);
         hoverAnim.run(hovered ? 1.0f : 0.0f);
 
-        renderer.text(setting.getDisplayName(), x + DropdownTheme.SETTING_PADDING_X, y + 1.0f, DropdownTheme.SETTING_TEXT_SCALE, DropdownTheme.settingLabel());
+        renderer.text(setting.getDisplayName(), DropdownTheme.SETTING_PADDING_X, 1.0f, DropdownTheme.SETTING_TEXT_SCALE, DropdownTheme.settingLabel());
 
-        float fieldX = getFieldX();
-        float fieldY = getFieldY();
+        float fieldX = DropdownTheme.SETTING_PADDING_X;
+        float fieldY = DropdownTheme.SETTING_HEIGHT - 1.0f;
         float fieldW = getFieldWidth();
         float hover = hoverAnim.getValue();
         Color background = MD3Theme.lerp(MD3Theme.SECONDARY_CONTAINER, MD3Theme.PRIMARY_CONTAINER, hover * 0.6f);
