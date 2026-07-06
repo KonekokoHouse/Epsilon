5cb16f5e32780b54232cda24a2d735990e2d3419
liuliuliu0127 <xyyysonic@163.com>
2026-07-07 01:14:16 +0800

    删除mixin中的无用import

--- common/src/main/java/com/github/epsilon/mixins/MixinEntity.java
+++ common/src/main/java/com/github/epsilon/mixins/MixinEntity.java
@@ -8,14 +8,11 @@ import com.github.epsilon.modules.impl.movement.Velocity;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import static com.github.epsilon.Constants.mc;
