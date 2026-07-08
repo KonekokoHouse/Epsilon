7e1a3c20d7c145cfbcdc243d1fc364ea582949b2
liuliuliu0127 <xyyysonic@163.com>
2026-07-08 19:41:44 +0800

    Reapply "添加实体控制/飞行功能以及基于实体飞行的简易自动巡航功能 (#381)"
    This reverts commit 2f6d6867a0532c23c71ba93193be0d78dbb436ec.

--- common/src/main/java/com/github/epsilon/mixins/MixinEntity.java
+++ common/src/main/java/com/github/epsilon/mixins/MixinEntity.java
@@ -1,6 +1,7 @@
 package com.github.epsilon.mixins;
 
 import com.github.epsilon.events.bus.EventBus;
+import com.github.epsilon.events.impl.EntityMoveEvent;
 import com.github.epsilon.events.impl.RaytraceEvent;
 import com.github.epsilon.events.impl.StrafeEvent;
 import com.github.epsilon.modules.impl.movement.Velocity;
@@ -14,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import static com.github.epsilon.Constants.mc;
@@ -59,6 +61,16 @@ public class MixinEntity {
                 args.set(2, 0.0);
             }
         }
+    }
+
+    @ModifyVariable(method = "move", at = @At("HEAD"), argsOnly = true)
+    private Vec3 onMoveModifyVariable(Vec3 delta) {
+        Entity self = (Entity) (Object) this;
+        if (net.minecraft.client.Minecraft.getInstance().player == null) return delta;
+        if (self == net.minecraft.client.Minecraft.getInstance().player) return delta;
+        EntityMoveEvent event = new EntityMoveEvent(self, delta);
+        EventBus.INSTANCE.post(event);
+        return event.movement;
     }
 
 }
