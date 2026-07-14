ef292d43463d693e24ae9e617982b842aefb200f
L3MonKe178 <hhcrystal2008@gmail.com>
2026-07-14 21:41:46 +0800

    Strafe

--- common/src/main/java/com/github/epsilon/modules/impl/movement/Speed.java
+++ common/src/main/java/com/github/epsilon/modules/impl/movement/Speed.java
@@ -14,12 +14,19 @@ import com.github.epsilon.settings.impl.IntSetting;
 import com.github.epsilon.utils.player.MoveUtils;
 import com.github.epsilon.utils.player.PlayerUtils;
 import com.github.epsilon.utils.timer.TimerUtils;
+import net.minecraft.network.protocol.game.ClientboundExplodePacket;
+import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket;
+import net.minecraft.network.protocol.game.ClientboundPlayerRotationPacket;
+import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
 import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.entity.vehicle.boat.Boat;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec2;
-import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.Vec3;
 
 public class Speed extends Module {
 
@@ -36,16 +43,16 @@ public class Speed extends Module {
     }
 
     private final EnumSetting<Mode> mode = enumSetting("Mode", Mode.Strafe);
-    public final DoubleSetting collideSpeed = doubleSetting("CollideSpeed", 0.08, 0, 0.08, 0.01, () -> mode.is(Mode.Grim));
+    private final DoubleSetting collideSpeed = doubleSetting("CollideSpeed", 0.08, 0, 0.08, 0.01, () -> mode.is(Mode.Grim));
     private final BoolSetting strict = boolSetting("Strict", true, () -> mode.is(Mode.Grim));
     private final BoolSetting boat = boolSetting("BoatLongJump", true, () -> mode.is(Mode.Grim));
-    public final DoubleSetting boatExpand = doubleSetting("BoatExpand", 0.2, 0, 1, 0.01, () -> mode.is(Mode.Grim));
-    public final DoubleSetting boatSpeed = doubleSetting("BoatSpeed", 0.2, -2, 2, 0.01, () -> mode.is(Mode.Grim));
-    public final DoubleSetting boatJump = doubleSetting("BoatJump", 0.2, 0, 2, 0.01, () -> mode.is(Mode.Grim));
+    private final DoubleSetting boatExpand = doubleSetting("BoatExpand", 0.2, 0, 1, 0.01, () -> mode.is(Mode.Grim));
+    private final DoubleSetting boatSpeed = doubleSetting("BoatSpeed", 0.2, -2, 2, 0.01, () -> mode.is(Mode.Grim));
+    private final DoubleSetting boatJump = doubleSetting("BoatJump", 0.2, 0, 2, 0.01, () -> mode.is(Mode.Grim));
 
     private final BoolSetting inWater = boolSetting("InWater", false, () -> !mode.is(Mode.Grim));
     private final BoolSetting inBlock = boolSetting("InBlock", false, () -> !mode.is(Mode.Grim));
-    private final BoolSetting airStop = boolSetting("AirStop", false, () -> !mode.is(Mode.Grim));
+    private final BoolSetting airStop = boolSetting("AirStop", true, () -> !mode.is(Mode.Grim));
     private final DoubleSetting lagTime = doubleSetting("LagTime", 500, 0, 1000, 1, () -> !mode.is(Mode.Grim));
 
     private final BoolSetting jump = boolSetting("Jump", true, () -> mode.is(Mode.Strafe));
@@ -76,16 +83,17 @@ public class Speed extends Module {
             speed = getSpeed(false);
             distance = getDistance2D();
         }
-
         stage = 4;
     }
 
     @EventHandler(priority = EventPriority.HIGH)
     private void onPacketReceive(PacketEvent.Receive event) {
+        if (nullCheck()) return;
+
         if (mode.is(Mode.Strafe)) {
-            if (event.getPacket() instanceof EntityVelocityUpdateS2CPacket packet) {
-                if (mc.player != null && packet.getEntityId() == mc.player.getId() && this.velocity.getValue()) {
-                    double speed = Math.sqrt(packet.getVelocityX() * packet.getVelocityX() + packet.getVelocityZ() * packet.getVelocityZ());
+            if (event.getPacket() instanceof ClientboundSetEntityMotionPacket(int id, Vec3 packetMovement)) {
+                if (id == mc.player.getId() && this.velocity.getValue()) {
+                    double speed = Math.hypot(packetMovement.x, packetMovement.z);
 
                     this.lastExp = this.expTimer
                             .passedMillise(this.coolDown.getValue())
@@ -99,14 +107,15 @@ public class Speed extends Module {
                         this.distance += this.lastExp * this.multiplier.getValue();
 
                         if (mc.player.getDeltaMovement().y > 0 && this.vertical.getValue() != 0) {
-                            mc.player.setDeltaMovement(0.0, mc.player.getDeltaMovement().y * this.vertical.getValue(), 0.0);
+                            setMotionY(mc.player.getDeltaMovement().y * this.vertical.getValue());
                         }
                     }
                 }
-            } else if (event.getPacket() instanceof ExplosionS2CPacket packet) {
+            } else if (event.getPacket() instanceof ClientboundExplodePacket packet) {
                 if (this.explosions.getValue()) {
-                    if (mc.player.position().distanceTo(new Vec3d(packet.getX(), packet.getY(), packet.getZ())) < 15) {
-                        double speed = Math.sqrt(packet.getPlayerVelocityX() * packet.getPlayerVelocityX() + packet.getPlayerVelocityZ() * packet.getPlayerVelocityZ());
+                    if (mc.player.position().distanceTo(packet.center()) < 15) {
+                        Vec3 knockback = packet.playerKnockback().orElse(Vec3.ZERO);
+                        double speed = Math.hypot(knockback.x, knockback.z);
                         this.lastExp = this.expTimer.passedMillise(this.coolDown.getValue()) ? speed : (speed - this.lastExp);
 
                         if (this.lastExp > 0) {
@@ -116,44 +125,37 @@ public class Speed extends Module {
                             this.distance += this.lastExp * this.multiplier.getValue();
 
                             if (mc.player.getDeltaMovement().y > 0) {
-                                mc.player.setDeltaMovement(0.0, mc.player.getDeltaMovement().y * this.vertical.getValue(), 0.0);
+                                setMotionY(mc.player.getDeltaMovement().y * this.vertical.getValue());
                             }
                         }
                     }
                 }
             }
         }
-        if (event.getPacket() instanceof PlayerPositionLookS2CPacket) {
+        if (event.getPacket() instanceof ClientboundPlayerPositionPacket || event.getPacket() instanceof ClientboundPlayerRotationPacket) {
             lagTimer.reset();
             resetStrafe();
         }
     }
 
     @EventHandler
-    private void onMove(MovedEvent event) {
-        double dx = mc.player.getX() - mc.player.xo;
-        double dz = mc.player.getZ() - mc.player.zo;
-        distance = Math.sqrt(dx * dx + dz * dz);
-    }
-
-    @EventHandler
-    private void onUpdate(PlayerTickEvent.Pre event) {
+    private void onPlayerTickPre(PlayerTickEvent.Pre event) {
         if (mode.is(Mode.Grim)) {
             if (!MoveUtils.isMoving()) {
                 return;
             }
 
             int collisions = 0;
-            AABB box = strict.getValue() ? mc.player.getBoundingBox() : mc.player.getBoundingBox().expand(1.0);
+            AABB box = strict.getValue() ? mc.player.getBoundingBox() : mc.player.getBoundingBox().inflate(1.0);
 
             for (Entity entity : mc.level.entitiesForRendering()) {
                 AABB entityBox = entity.getBoundingBox();
-                if (boat.getValue() && mc.player.isOnGround() && entity instanceof BoatEntity && box.intersects(entityBox.expand(boatExpand.getValue()))) {
+                if (boat.getValue() && mc.player.onGround() && entity instanceof Boat && box.intersects(entityBox.inflate(boatExpand.getValue()))) {
                     double yaw = Math.toRadians(getSprintYaw(mc.player.getYRot()));
                     double boost = boatSpeed.getValue();
                     mc.player.setDeltaMovement(-Math.sin(yaw) * boost, boatJump.getValue(), Math.cos(yaw) * boost);
                     return;
-                } else if (box.intersects(entityBox) && canCauseSpeed(entity)) {
+                } else if (entity != mc.player && entity instanceof LivingEntity && !(entity instanceof ArmorStand) && box.intersects(entityBox)) {
                     collisions++;
                 }
             }
@@ -164,8 +166,9 @@ public class Speed extends Module {
         }
     }
 
-    private boolean canCauseSpeed(Entity entity) {
-        return entity != mc.player && entity instanceof LivingEntity && !(entity instanceof ArmorStandEntity);
+    @EventHandler
+    private void onPlayerTickPost(PlayerTickEvent.Post event) {
+        if (!nullCheck()) distance = getDistance2D();
     }
 
     @EventHandler
@@ -174,8 +177,8 @@ public class Speed extends Module {
             mc.player.setDeltaMovement(0.0, mc.player.getDeltaMovement().y, 0.0);
         }
         if (!this.inWater.getValue() && (mc.player.isUnderWater() || mc.player.isInWater() || mc.player.isInLava())
-                || mc.player.isRiding()
-                || mc.player.isHoldingOntoLadder()
+                || mc.player.isPassenger()
+                || mc.player.onClimbable()
                 || !inBlock.getValue() && PlayerUtils.isInBlock()
                 || mc.player.getAbilities().flying
                 || mc.player.isFallFlying()
@@ -196,23 +199,21 @@ public class Speed extends Module {
 
             if (this.stage == 1) {
                 this.speed = 1.35 * getSpeed(this.slow.getValue(), this.strafeSpeed.getValue()) - 0.01;
-            } else if (this.stage == 2 && mc.player.onGround() && (mc.options.jumpKey.isPressed() || this.jump.getValue())) {
+            } else if (this.stage == 2 && mc.player.onGround() && (mc.options.keyJump.isDown() || this.jump.getValue())) {
                 double yMotion = 0.3999 + getJumpSpeed();
-                mc.player.setDeltaMovement(0.0, yMotion, 0.0);
+                setMotionY(yMotion);
                 event.setY(yMotion);
                 event.cancel();
                 this.speed = this.speed * (this.boost ? 1.6835 : 1.395);
             } else if (this.stage == 3) {
                 this.speed = this.distance - 0.66
-                        * (this.distance - (this.slow.getValue(), this.strafeSpeed.getValue()));
+                        * (this.distance - getSpeed(this.slow.getValue(), this.strafeSpeed.getValue()));
 
                 this.boost = !this.boost;
             } else {
                 if ((canCollide(null,
-                        mc.player
-                                .getBoundingBox()
-                                .offset(0.0, mc.player.getDeltaMovement().y, 0.0))
-                        || mc.player.collidedSoftly)
+                        mc.player.getBoundingBox().move(0.0, mc.player.getDeltaMovement().y, 0.0))
+                        || mc.player.minorHorizontalCollision)
                         && this.stage > 0) {
                     this.stage = 1;
                 }
@@ -222,16 +223,13 @@ public class Speed extends Module {
 
             this.speed = Math.min(this.speed, 10);
             this.speed = Math.max(this.speed, getSpeed(this.slow.getValue(), this.strafeSpeed.getValue()));
-            double n = mc.player.input.movementForward;
-            double n2 = mc.player.input.movementSideways;
-            double n3 = mc.player.getYaw();
+            Vec2 moveVector = mc.player.input.getMoveVector();
+            double n = moveVector.y;
+            double n2 = moveVector.x;
+            double n3 = mc.player.getYRot();
             if (n == 0.0 && n2 == 0.0) {
                 event.setX(0.0);
                 event.setZ(0.0);
-                event.cancel();
-            } else if (n != 0.0 && n2 != 0.0) {
-                n *= Math.sin(0.7853981633974483);
-                n2 *= Math.cos(0.7853981633974483);
             }
             event.setX((n * this.speed * -Math.sin(Math.toRadians(n3)) + n2 * this.speed * Math.cos(Math.toRadians(n3))) * 0.99);
             event.setZ((n * this.speed * Math.cos(Math.toRadians(n3)) - n2 * this.speed * -Math.sin(Math.toRadians(n3))) * 0.99);
@@ -242,18 +240,18 @@ public class Speed extends Module {
         }
         double speedEffect = 1.0;
         double slowEffect = 1.0;
-        if (mc.player.hasStatusEffect(StatusEffects.SPEED)) {
-            double amplifier = mc.player.getStatusEffect(StatusEffects.SPEED).getAmplifier();
+        if (mc.player.hasEffect(MobEffects.SPEED)) {
+            double amplifier = mc.player.getEffect(MobEffects.SPEED).getAmplifier();
             speedEffect = 1 + (0.2 * (amplifier + 1));
         }
-        if (mc.player.hasStatusEffect(StatusEffects.SLOWNESS)) {
-            double amplifier = mc.player.getStatusEffect(StatusEffects.SLOWNESS).getAmplifier();
+        if (mc.player.hasEffect(MobEffects.SLOWNESS)) {
+            double amplifier = mc.player.getEffect(MobEffects.SLOWNESS).getAmplifier();
             slowEffect = 1 + (0.2 * (amplifier + 1));
         }
         final double base = 0.2873f * speedEffect / slowEffect;
         float jumpEffect = 0.0f;
-        if (mc.player.hasStatusEffect(StatusEffects.JUMP_BOOST)) {
-            jumpEffect += (mc.player.getStatusEffect(StatusEffects.JUMP_BOOST).getAmplifier() + 1) * 0.1f;
+        if (mc.player.hasEffect(MobEffects.JUMP_BOOST)) {
+            jumpEffect += (mc.player.getEffect(MobEffects.JUMP_BOOST).getAmplifier() + 1) * 0.1f;
         }
 
         if (mode.getValue() == Mode.StrafeStrict) {
@@ -263,19 +261,19 @@ public class Speed extends Module {
             if (strafe == 1) {
                 speed = 1.35f * base - 0.01f;
             } else if (strafe == 2) {
-                if (mc.player.input.jumping || !mc.player.onGround()) {
+                if (mc.player.input.keyPresses.jump() || !mc.player.onGround()) {
                     return;
                 }
                 float jump = 0.3999999463558197f + jumpEffect;
                 event.setY(jump);
                 event.cancel();
-                mc.player.setDeltaMovement(mc.player.getDeltaMovement().x, jump, mc.player.getDeltaMovement().z);
+                setMotionY(jump);
                 speed *= 2.149;
             } else if (strafe == 3) {
                 double moveSpeed = 0.66 * (distance - base);
                 speed = distance - moveSpeed;
             } else {
-                if ((!mc.level.isSpaceEmpty(mc.player, mc.player.getBoundingBox().offset(0,
+                if ((!mc.level.noCollision(mc.player, mc.player.getBoundingBox().move(0,
                         mc.player.getDeltaMovement().y, 0)) || mc.player.verticalCollision) && strafe > 0) {
                     strafe = 1;
                 }
@@ -297,25 +295,14 @@ public class Speed extends Module {
         }
     }
 
-    public Vec2 handleStrafeMotion(final float speed) {
-        float forward = mc.player.input.movementForward;
-        float strafe = mc.player.input.movementSideways;
-        float yaw = mc.player.prevYaw + (mc.player.getYaw() - mc.player.prevYaw) * mc.getRenderTickCounter().getTickDelta(true);
+    private Vec2 handleStrafeMotion(final float speed) {
+        Vec2 moveVector = mc.player.input.getMoveVector();
+        float forward = moveVector.y;
+        float strafe = moveVector.x;
+        float tickDelta = mc.getDeltaTracker().getGameTimeDeltaPartialTick(true);
+        float yaw = Mth.lerp(tickDelta, mc.player.yRotO, mc.player.getYRot());
         if (forward == 0.0f && strafe == 0.0f) {
             return Vec2.ZERO;
-        } else if (forward != 0.0f) {
-            if (strafe >= 1.0f) {
-                yaw += forward > 0.0f ? -45 : 45;
-                strafe = 0.0f;
-            } else if (strafe <= -1.0f) {
-                yaw += forward > 0.0f ? 45 : -45;
-                strafe = 0.0f;
-            }
-            if (forward > 0.0f) {
-                forward = 1.0f;
-            } else if (forward < 0.0f) {
-                forward = -1.0f;
-            }
         }
         float rx = (float) Math.cos(Math.toRadians(yaw));
         float rz = (float) -Math.sin(Math.toRadians(yaw));
@@ -329,12 +316,12 @@ public class Speed extends Module {
 
     private double getSpeed(boolean slowness, double defaultSpeed) {
         if (mc.player.hasEffect(MobEffects.SPEED)) {
-            int amplifier = mc.player.getActiveEffectsMap().get(MobEffects.SPEED).getAmplifier();
+            int amplifier = mc.player.getEffect(MobEffects.SPEED).getAmplifier();
             defaultSpeed *= 1.0 + 0.2 * (amplifier + 1);
         }
 
         if (slowness && mc.player.hasEffect(MobEffects.SLOWNESS)) {
-            int amplifier = mc.player.getActiveEffectsMap().get(MobEffects.SLOWNESS).getAmplifier();
+            int amplifier = mc.player.getEffect(MobEffects.SLOWNESS).getAmplifier();
             defaultSpeed /= 1.0 + 0.2 * (amplifier + 1);
         }
 
@@ -348,7 +335,7 @@ public class Speed extends Module {
         double defaultSpeed = 0.0;
 
         if (mc.player.hasEffect(MobEffects.JUMP_BOOST)) {
-            int amplifier = mc.player.getActiveEffectsMap().get(MobEffects.JUMP_BOOST).getAmplifier();
+            int amplifier = mc.player.getEffect(MobEffects.JUMP_BOOST).getAmplifier();
             defaultSpeed += (amplifier + 1) * 0.1;
         }
 
@@ -356,15 +343,12 @@ public class Speed extends Module {
     }
 
     private boolean canCollide(Entity entity, AABB box) {
-        BlockCollisionSpliterator<VoxelShape> blockCollisionSpliterator = new BlockCollisionSpliterator<>(mc.level, entity, box, false, (pos, voxelShape) -> voxelShape);
+        return !mc.level.noBlockCollision(entity, box);
+    }
 
-        do {
-            if (!blockCollisionSpliterator.hasNext()) {
-                return false;
-            }
-        } while (blockCollisionSpliterator.next().isEmpty());
-
-        return true;
+    private void setMotionY(double y) {
+        Vec3 movement = mc.player.getDeltaMovement();
+        mc.player.setDeltaMovement(movement.x, y, movement.z);
     }
 
     private double getDistance2D() {
@@ -374,22 +358,22 @@ public class Speed extends Module {
     }
 
     private float getSprintYaw(float yaw) {
-        if (mc.options.forwardKey.isPressed() && !mc.options.backKey.isPressed()) {
-            if (mc.options.leftKey.isPressed() && !mc.options.rightKey.isPressed()) {
+        if (mc.options.keyUp.isDown() && !mc.options.keyDown.isDown()) {
+            if (mc.options.keyLeft.isDown() && !mc.options.keyRight.isDown()) {
                 yaw -= 45f;
-            } else if (mc.options.rightKey.isPressed() && !mc.options.leftKey.isPressed()) {
+            } else if (mc.options.keyRight.isDown() && !mc.options.keyLeft.isDown()) {
                 yaw += 45f;
             }
-        } else if (mc.options.backKey.isPressed() && !mc.options.forwardKey.isPressed()) {
+        } else if (mc.options.keyDown.isDown() && !mc.options.keyUp.isDown()) {
             yaw += 180f;
-            if (mc.options.leftKey.isPressed() && !mc.options.rightKey.isPressed()) {
+            if (mc.options.keyLeft.isDown() && !mc.options.keyRight.isDown()) {
                 yaw += 45f;
-            } else if (mc.options.rightKey.isPressed() && !mc.options.leftKey.isPressed()) {
+            } else if (mc.options.keyRight.isDown() && !mc.options.keyLeft.isDown()) {
                 yaw -= 45f;
             }
-        } else if (mc.options.leftKey.isPressed() && !mc.options.rightKey.isPressed()) {
+        } else if (mc.options.keyLeft.isDown() && !mc.options.keyRight.isDown()) {
             yaw -= 90f;
-        } else if (mc.options.rightKey.isPressed() && !mc.options.leftKey.isPressed()) {
+        } else if (mc.options.keyRight.isDown() && !mc.options.keyLeft.isDown()) {
             yaw += 90f;
         }
         return Mth.wrapDegrees(yaw);
