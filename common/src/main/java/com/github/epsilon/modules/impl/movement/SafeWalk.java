9c88e995fefa782ebefed4c75e487f143f70429f
L3MonKe178 <hhcrystal2008@gmail.com>
2026-07-16 18:06:05 +0800

    Remove first

--- common/src/main/java/com/github/epsilon/modules/impl/movement/SafeWalk.java
+++ common/src/main/java/com/github/epsilon/modules/impl/movement/SafeWalk.java
@@ -2,8 +2,6 @@ package com.github.epsilon.modules.impl.movement;
 
 import com.github.epsilon.modules.Category;
 import com.github.epsilon.modules.Module;
-import com.github.epsilon.settings.impl.BoolSetting;
-import net.minecraft.world.item.BlockItem;
 
 public class SafeWalk extends Module {
 
@@ -11,15 +9,6 @@ public class SafeWalk extends Module {
 
     private SafeWalk() {
         super("Safe Walk", Category.MOVEMENT);
-    }
-
-    private final BoolSetting onlyHoldingBlock = boolSetting("Only Holding Block", false);
-    private final BoolSetting onlyBack = boolSetting("Only Backward", true);
-
-    public boolean shouldSafeWalk() {
-        boolean holdingBlock = mc.player.getInventory().getSelectedItem().getItem() instanceof BlockItem || !onlyHoldingBlock.getValue();
-        boolean back = mc.options.keyDown.isDown() || !onlyBack.getValue();
-        return isEnabled() && holdingBlock && back;
     }
 
 }
